6551531ba93b0cea6a3239ba5964e104b42f323a
Thomas Pichler <thomas.pichler@woodmark.de>
2023-10-12 12:06:56 +0200

    test

---
+++ test 2.py
@@ -0,0 +1,18 @@
+"""
+:modulename: test.py
+:moduleauthor: Thomas
+
+This notebook is only a test
+
+=======
+"""
+
+print("hello world")
+
+TEST_CONST = 42
+
+test_v = input("Bitte Eingabe ")
+
+print(test_v + test_v)
+
+# test123
