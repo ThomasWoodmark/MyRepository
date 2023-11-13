c5db506582b544d813704f072c9a1818c31fc1b3
tpichle <thomas.pichler@woodmark.de>
2023-11-13 16:23:54 +0100

    test

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ This notebook is only a test
 
 print("hello world")
 
-TEST_CONST = 42355
+TEST_CONST = 4235555
 
-test_y = input("Bitte Eingabe ")
+test_y4 = input("Bitte Eingabe ")
 
 print(test_y + test_y)
 
